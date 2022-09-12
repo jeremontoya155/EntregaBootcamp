2c4d2a5933eda6891d98ec7ddb20f38d53ec6786
Jeremias Montoya <83260187+jeremontoya155@users.noreply.github.com>
2022-09-12 10:14:38 -0300

    Primera Entrega

---
+++ src/main/java/test/entregauno/EntregaUno.java
@@ -0,0 +1,23 @@
+
+package test.entregauno;
+
+
+public class EntregaUno {
+
+    public static void main(String[] args) {
+        
+        System.out.println(1);
+        System.out.println(1.5);
+        float valor=15F;
+        System.out.println(valor);
+        long valorDos=12L;
+        System.out.println(valorDos);
+        System.out.println(true);
+        System.out.println("String ejemplar");
+        System.out.println('A');
+        short valorTres=1;
+        System.out.println(valorTres);
+        byte valorCuatro=1;
+        System.out.println(valorCuatro);
+    }   
+}
